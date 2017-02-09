26880fa638e1943bc438fef76b609e601609c512
adam aulia rahmadi <adamaulia242@gmail.com>
2017-02-09 23:07:57 +0700

    update stopword, add tweet_user feature

--- pilgub_simplified.py
+++ pilgub_simplified.py
@@ -11,6 +11,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from collections import Counter
+import operator
 
 def preprocessing_tweet(tweet):
     
@@ -48,30 +50,65 @@ def preprocessing_tweet(tweet):
     return ' '.join(tw_sw_removal)
     #return tw_remove_puc
     
+def get_user_tweet(tweet):
+    if isinstance(tweet, unicode):
+        tweet = tweet.encode('utf-8', 'replace')
+    if isinstance(tweet, (float, int, complex, long)):
+        tweet = str(tweet)
+    user = re.findall(r"@[\w]*",tweet)
+    
+    return user
+    
+def tweet_viz(final_tweet,paslon):
+    df = pd.DataFrame(final_tweet[paslon].items())
+    plt.figure()
+    sns.plt.suptitle(final_paslon[paslon])
+    sns.barplot(x=0,y=1,data=df)
+    
+def tweet_user(user_tweet,paslon,most_user):
+    user_tmp = dict(Counter(user_tweet[paslon]))
+    user_sort = sorted(user_tmp.items(), key=operator.itemgetter(1))
+    df_user = pd.DataFrame(dict(user_sort[-most_user:]).items())
+    sns.plt.suptitle(final_paslon[paslon])
+    sns.barplot(x=0,y=1,data=df_user)
+    return 0
 
+print 'load dataset'    
 dataset=pd.read_excel('dataset.xlsx')
-tweet=dataset['Tweet'].tolist()
+tweet_raw=dataset['Tweet'].tolist()
 date=dataset['Time'].tolist()
 
 stopword= [line.rstrip('\n') for line in open('stopword.txt')]
 print 'processing'
 clean_tweet = []
-for idx,item in enumerate(tweet):
+for idx,item in enumerate(tweet_raw):
     #print idx
     clean_tweet.append(preprocessing_tweet(item))
 
-         
+print 'Tfidf'         
 Tfidf = TfidfVectorizer(norm='l2',min_df=0, use_idf=True, smooth_idf=True, sublinear_tf=True)
 Tfidf.fit(clean_tweet)
 
+print 'Testing'
 most_tweet=20
 final_tweet = []
 final_paslon = []
+user_tweet = []
 for tweet in dataset.groupby('Paslon'):
+    user = []
     prep_twt = tweet[1]['Tweet'].tolist()
     #print 'twt',prep_twt
     tmp = [preprocessing_tweet(item) for item in prep_twt]
-        
+           
+    # get user tweet based on paslong       
+    tmp_user = [get_user_tweet(item) for item in prep_twt]            
+    for item in tmp_user:
+        if item:
+            for user_twt in item:
+                user.append(user_twt)
+    
+    user_tweet.append(user)        
+           
     tw_by_paslon=np.sum(Tfidf.transform(tmp).toarray(),axis=0)
     sort_idx=np.argsort(tw_by_paslon.flatten())[::-1]
     most_paslon_tweet=[(Tfidf.get_feature_names()[item],tw_by_paslon[item]) for item in sort_idx.tolist()[:most_tweet]]
@@ -79,16 +116,7 @@ for tweet in dataset.groupby('Paslon'):
     final_tweet.append(dict(most_paslon_tweet))
     final_paslon.append(tweet[0])
 
-# change 0 for AgusSylvi, 1 AhokDjarot, 2 AniesSandi
-df = pd.DataFrame(final_tweet[1].items())
-sns.plt.suptitle(final_paslon[1])
-sns.barplot(x=0,y=1,data=df)     
 
-#==============================================================================
-# fig =  plt.figure()
-# for idx,item in enumerate(final_tweet):
-#     df = pd.DataFrame(item.items())
-#     ax = fig.add_subplot(211)
-#     sns.plt.suptitle(final_paslon[idx])
-#     sns.barplot(x=0,y=1,data=df)
-#==============================================================================
+# change 0 for AgusSylvi, 1 AhokDjarot, 2 AniesSandi
+#tweet_viz(final_tweet,2)
+tweet_user(user_tweet,2,10)
